120178b7b3d0981f3f696e4fff74b5c6b41d63ad
voiceofgrog <voiceofgrog@gmail.com>
2021-06-14 16:35:41 -0400

    Newest changes

--- js/app.js
+++ js/app.js
@@ -1,44 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
+// Letting me know the page finished loading
+console.log('Your Page Loaded');
 
-    // Letting me know the page finished loading
-    console.log('Your Page Loaded');
+// Get the mask-graphic-caption DIV
+const maskCaption = document.querySelector('.landing-page__title-mask-graphic-caption');
 
-    // Get the mask-graphic-caption DIV
-    const maskCaption = document.querySelector('.landing-page__title-mask-graphic-caption');
+// Get the tagline DIV
+const tagline = document.querySelector('.landing-page__tagline');
 
-    // Get the tagline DIV
-    const tagline = document.querySelector('.landing-page__tagline');
-    const taglineSource = document.querySelector('.landing-page__tagline-source');
+// Get the taglineSource DIV
+const taglineSource = document.querySelector('.landing-page__tagline-source');
 
-    // Pull a random tagline from the array    
-    const randomQuote = function (array) {
-        let randomItem = array[Math.floor(Math.random() * array.length)];
-        return array[randomItem.quote];
-    }
+// Pull a random tagline from the array    
+const randomQuote = function (array) {
+    let randomItem = array[Math.floor(Math.random() * array.length)];
+    return array[randomItem.quote];
+}
 
-    // Fetching the quotes JSON File
-    const getQuotes = async () => {
+// Getting the quotes JSON File
+// NOTE: How handle errors and response status
+async function getQuotes() {
 
-        const response = await fetch('./data/quotes.json');
-        const data = await response.json();
-        
-        // Get random item from data
-        const currentQuote = data[Math.floor(Math.random() * data.length)];
+    const response = await fetch('./data/quotes.json');
+    const data = await response.json();
 
-        // Place the random tagline into the landing-page__tagline DIV
-        tagline.innerText = `${currentQuote.quote}`;
-        // Place the tagline source into the landing-page__tagline-source DIV
-        console.log(taglineSource);
-        taglineSource.innerText = `- ${currentQuote.source}`;
+    // Get random item from data
+    const currentQuote = data[Math.floor(Math.random() * data.length)];
 
-    };
+    // Place the random tagline into the landing-page__tagline DIV
+    tagline.innerText = `${currentQuote.quote}`;
+    // Place the tagline source into the landing-page__tagline-source DIV
+    console.log(taglineSource);
+    taglineSource.innerText = `- ${currentQuote.source}`;
 
-    // Initiate fetch for a quote from the JSON file
-    getQuotes();
+}
 
-    // Load the mask-graphic-caption DIV after 3 seconds
-    setTimeout(function () {
-        maskCaption.style.opacity = '1'
-    }, 3000);
+// Initiate fetch for a quote from the JSON file
+getQuotes();
 
-});
+// Load the mask-graphic-caption DIV after 3 seconds
+setTimeout(function () {
+    maskCaption.style.opacity = '1'
+}, 3000);
